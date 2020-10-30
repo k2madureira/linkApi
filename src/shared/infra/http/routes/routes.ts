49c6cb6229b4fef397adb98a5de5e7d495818632
k2madureira <lenilsonmadureira@gmail.com>
2020-10-30 14:29:36 -0300

    routes

--- src/shared/infra/http/routes/routes.ts
+++ src/shared/infra/http/routes/routes.ts
@@ -1,9 +1,25 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 import orderRouter from '@modules/Order/infra/http/routes/order.routes';
 
 const routes = Router();
 
+routes.use('/', (request: Request, response: Response) => {
+  return response.json({
+    routes: {
+      order: {
+        list: {
+          path: '/order/all',
+          type: 'get',
+        },
+        create: {
+          path: '/order',
+          type: 'post',
+        },
+      },
+    },
+  });
+});
 routes.use('/order', orderRouter);
 
 export default routes;
